15c7bd76dec5cb1d3ac94d7bcde306e2143f1088
RamphoreTM <1908649@students.wits.ac.za>
2020-04-25 12:53:45 +0200

    added scrollTo functions

--- app/src/androidTest/java/com/example/textfileapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/textfileapplication/ExampleInstrumentedTest.java
@@ -155,8 +155,8 @@ public class ExampleInstrumentedTest {
         closeSoftKeyboard();
         fileContent.perform(typeText("poem"));
         closeSoftKeyboard();
-        Save.perform(click());
-        Delete.perform(click());
+        Save.perform(scrollTo()).perform(click());
+        Delete.perform(scrollTo()).perform(click());
         Display.check(matches(withText(fileDeleted)));
     }
 
