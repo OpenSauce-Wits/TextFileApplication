0cb32602606854a0ec318cfbdc9ed3c47b41ff67
RamphoreTM <1908649@students.wits.ac.za>
2020-06-14 19:40:38 +0200

    renamed test file and associated classes

--- app/src/androidTest/java/com/example/textfileapplication/MainActivityInstrumentedTest.java
+++ app/src/androidTest/java/com/example/textfileapplication/MainActivityInstrumentedTest.java
@@ -30,7 +30,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class MainActivityInstrumentedTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
